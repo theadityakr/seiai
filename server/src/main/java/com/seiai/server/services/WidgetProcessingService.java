86e1fd1ec388c881f42ccc1ff01161d58b8839a6
theadityakr <theaditykr@gmail.com>
2025-04-02 03:22:13 -0400

    code color, format

--- server/src/main/java/com/seiai/server/services/WidgetProcessingService.java
+++ server/src/main/java/com/seiai/server/services/WidgetProcessingService.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
-import java.util.Optional;
 
 @Service
 public class WidgetProcessingService {
